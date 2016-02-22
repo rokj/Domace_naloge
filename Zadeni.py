144065e8ab3bf5091c236f6158609db25ac8e65a
Klemen1975 <rokj@rasca.net>
2016-02-22 08:27:03 +0100

    zadeni

---
+++ Zadeni.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+num = raw_input("Vpiši številko:")
+
+if num == "5":
+    print ("Zadetek!!!, zadel si številko, dobiš 1 mio")
+
+else:
+    print ("Vec sreče prihodnjič")
