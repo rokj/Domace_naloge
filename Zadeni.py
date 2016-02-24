b5930d2fb35f9ad89dec6d6e1a3c7735efcd572a
Klemen P <kpeklaj@gmail.com>
2016-02-25 00:19:30 +0100

    Delete Zadeni.py

--- Zadeni.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-num = raw_input("Vpiši številko:")
-
-if num == "5":
-    print ("Zadetek!!!, zadel si številko, dobiš 1 mio")
-
-else:
-    print ("Vec sreče prihodnjič")
